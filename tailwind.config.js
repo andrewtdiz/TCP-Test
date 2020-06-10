2167c1b4a05cba40bfdbcad2aca215a14cd4775d
ChrisDizenzo <cddizenzo4@gmail.com>
2020-06-10 18:58:51 -0400

    newest2

--- tailwind.config.js
+++ tailwind.config.js
@@ -149,6 +149,8 @@ module.exports = {
       '48': '12rem',
       '56': '14rem',
       '64': '16rem',
+      '72': '18rem',
+      '80':'20rem',
     },
     backgroundColor: theme => theme('colors'),
     backgroundOpacity: theme => theme('opacity'),
